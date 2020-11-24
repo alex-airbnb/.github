e83bb7ad3c3db987da59c08fd61d5dfeb48dd751
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2020-11-23 20:35:43 -0600

    feat: Enable issues in the alex_airbnb repository.

--- main.tf
+++ main.tf
@@ -8,5 +8,5 @@ module "alex_airbnb_repository" {
   repository_name        = "alex_airbnb"
   repository_description = "The principal repository of the alex_airbnb project."
   repository_topics      = ["circleci", "terraform"]
-  repository_has_issues  = false
+  repository_has_issues  = true
 }
