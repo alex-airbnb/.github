433efe96f5a687bfb6b1dac018a765d68220cdd1
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2021-02-16 20:28:20 -0600

    feat: Add account_api_repository.

--- main.tf
+++ main.tf
@@ -10,3 +10,11 @@ module "alex_airbnb_repository" {
   repository_topics      = ["circleci", "terraform"]
   repository_has_issues  = true
 }
+
+module "account_api_repository" {
+  source                 = "./modules/alex_airbnb_repository"
+  repository_name        = "account_api"
+  repository_description = "Account API for the Account Bounded Context."
+  repository_topics      = ["circleci", "golang"]
+  repository_has_issues  = false
+}
